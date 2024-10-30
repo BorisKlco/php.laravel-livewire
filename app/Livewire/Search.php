2779d478a7bb96185e0633ebc4b47d319f64cbad
BorisKlco <klco@pm.me>
2024-10-30 09:19:09 +0100

    search

--- app/Livewire/Search.php
+++ app/Livewire/Search.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\Article;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Search extends Component
 {
+    #[Validate('required')]
+    public $userSearch = '';
+    public $articles = [];
+
+    public function updatedUserSearch($value)
+    {
+        $this->reset('articles');
+        $this->articles = Article::where('title', 'LIKE', "%{$value}%")->limit(5)->get();
+    }
+
+    public function clear()
+    {
+        $this->reset('userSearch', 'articles');
+    }
+
     public function render()
     {
         return view('livewire.search');
